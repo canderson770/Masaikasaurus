8c3e6833f10393a2c9e477d8c6fccc334fec4a95
Unknown <rp17000@gmail.com>
2017-04-24 19:01:39 -0600

    masiakasaurus unity test

---
+++ Unity/Masiakasaurus test/Assets/AnimationController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour 
+{
+	Animator anim;
+	float leftCurrent = 0;
+	float rightCurrent = 0;
+
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+	}
+	
+	void Update ()
+	{
+		if (Input.GetAxis ("Horizontal") < 0) 
+		{
+			anim.SetLayerWeight(1, 0);
+			anim.SetLayerWeight (2, Mathf.Abs((Input.GetAxis("Horizontal")))/2);
+			print ("left");
+		}
+		else if (Input.GetAxis ("Horizontal") > 0) 
+		{
+			anim.SetLayerWeight(1, Mathf.Abs((Input.GetAxis("Horizontal")))/2);
+			anim.SetLayerWeight (2, 0);
+			print ("right");
+		}
+		else
+		{
+			anim.SetLayerWeight(1, Mathf.Abs((Input.GetAxis("Horizontal"))));
+			anim.SetLayerWeight (2, Mathf.Abs((Input.GetAxis("Horizontal"))));
+		}
+	}
+}
---
+++ Unity/Masiakasaurus test/Assets/CharacterMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour 
+{
+	Rigidbody rb;
+	Animator anim;
+	public float defaultSpeed = 3;
+	private float speed = 0;
+	public float sprintSpeed = 0;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.LeftShift)) 
+		{
+			speed = sprintSpeed;
+		}
+		else
+			speed = defaultSpeed;
+		
+		if (Input.GetAxis ("Vertical") > 0.1f)
+		{
+			transform.Translate(Vector3.forward * speed * .015f);
+		}
+		else if (Input.GetAxis ("Vertical") < -0.1f) 
+		{
+			transform.Translate(Vector3.back * speed * .015f);
+		}
+		else 
+		{
+			speed = 0;
+		}
+
+		anim.SetFloat ("Speed", speed);
+
+
+
+
+		if (Input.GetAxis ("Horizontal") > 0.1f)
+		{
+			transform.Rotate(Vector3.up * speed / 3);
+		}
+		else if (Input.GetAxis ("Horizontal") < -0.1f) 
+		{
+			transform.Rotate(Vector3.down * speed / 3);
+		}
+	}
+}
