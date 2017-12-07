d16217032ee2870d9efcc4f77749b74204ce5dca
Unknown <rp17000@gmail.com>
2017-12-07 11:14:50 -0700

    toolbox 0.5

--- Maya_CyberDino/Scripting/Python/python/simple_functions.py
+++ Maya_CyberDino/Scripting/Python/python/simple_functions.py
@@ -1,5 +1,5 @@
 #simple_functions
-#2017-11-30
+#2017-12-07
 #============================================
 import maya.cmds as cmds
 import maya.mel as mel
@@ -9,7 +9,7 @@ def delete_history():
     cmds.delete(ch=True)
     
 def freeze_transformations():
-    cmds.makeIdentity(ctrl, apply=True)
+    cmds.makeIdentity(apply=True)
     
 def center_pivot():
     cmds.xform(centerPivots=True)
