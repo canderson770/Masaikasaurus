820f71603a743e4e59e3d2322f61893943cfedc3
Unknown <rp17000@gmail.com>
2017-12-14 10:57:31 -0700

    scripts .8

--- Maya_CyberDino/Scripting/Python/python/custom_outliner.py
+++ Maya_CyberDino/Scripting/Python/python/custom_outliner.py
@@ -1,5 +1,5 @@
 #custom_outliner
-#2017-12-10
+#2017-12-14
 #============================================
 import maya.cmds as cmds
 
@@ -10,14 +10,26 @@ def custom_outliner():
     if cmds.window(custom_outliner_window, exists=True):
         cmds.deleteUI(custom_outliner_window, window=True)
 
-    custom_outliner_window = cmds.window(custom_outliner_window, title='Custom Outliner', minimizeButton=False, maximizeButton=False)
+    custom_outliner_window = cmds.window(custom_outliner_window, title='Custom Outliner')
     
     custom_outliner_column = cmds.formLayout(parent=custom_outliner_window)
     
     custom_outliner_row_buttons = cmds.rowLayout(parent=custom_outliner_column, numberOfColumns=6)
     
-    custom_outliner_list = cmds.textScrollList(parent=custom_outliner_column, allowMultiSelection=True, 
-        deleteKeyCommand=lambda *args: outliner_remove(custom_outliner_list), selectCommand=lambda *args: outliner_select(custom_outliner_list))
+    custom_outliner_list_row = cmds.paneLayout(parent=custom_outliner_column, configuration='vertical2')
+    
+    type_name_list = ['transform', 'nurbsCurve', '--------------------------', 'joint',
+     'parentConstraint', 'scaleConstraint', 'ikHandle', 'mesh', 'camera', 
+     'cluster', '--------------------------', 'reverse']
+    
+    custom_outliner_select_list = cmds.textScrollList(parent=custom_outliner_list_row, allowMultiSelection=True, 
+        selectCommand=lambda *args: outliner_select_by_type(custom_outliner_select_list, custom_outliner_list))
+        
+    cmds.textScrollList(custom_outliner_select_list, e=True, append=type_name_list)
+    
+    custom_outliner_list = cmds.textScrollList(parent=custom_outliner_list_row, allowMultiSelection=True, 
+        deleteKeyCommand=lambda *args: outliner_remove(custom_outliner_list), doubleClickCommand=lambda *args: outliner_select_all(custom_outliner_list),
+        selectCommand=lambda *args: outliner_select(custom_outliner_list))
         
     custom_outliner_add_button = cmds.button(parent=custom_outliner_row_buttons, label='Add', 
         annotation='Add selection to outliner', command=lambda *args: outliner_add(custom_outliner_list))
@@ -33,40 +45,46 @@ def custom_outliner():
         annotation='Move selected item down', height=30, width=30, command=lambda *args: outliner_down(custom_outliner_list))
         
     cmds.formLayout( custom_outliner_column, edit=True, attachForm=[(custom_outliner_row_buttons, 'top', 5), (custom_outliner_row_buttons, 'left', 5),
-        (custom_outliner_list, 'bottom', 5), (custom_outliner_list, 'right', 5), (custom_outliner_list, 'left', 5)],
-        attachControl=[(custom_outliner_list, 'top', 0, custom_outliner_row_buttons)])
+        (custom_outliner_list_row, 'bottom', 5), (custom_outliner_list_row, 'right', 5), (custom_outliner_list_row, 'left', 5)],
+        attachControl=[(custom_outliner_list_row, 'top', 0, custom_outliner_row_buttons)])
         
-    cmds.window(custom_outliner_window, edit=True, widthHeight=(220, 570))
+    cmds.window(custom_outliner_window, edit=True, widthHeight=(400, 570))
     cmds.showWindow(custom_outliner_window)
 
 def outliner_select(custom_outliner_list):
     select_items = cmds.textScrollList(custom_outliner_list, q=True, selectItem=True)
     cmds.select(select_items)
+    
+def outliner_select_all(custom_outliner_list):
+    current_list = cmds.textScrollList(custom_outliner_list, q=True, allItems=True)
+    cmds.textScrollList(custom_outliner_list, e=True, selectItem=current_list)
+    outliner_select(custom_outliner_list)
 
 def outliner_add(custom_outliner_list):
     sels = cmds.ls(sl=True)
-    cmds.textScrollList(custom_outliner_list, e=True, append=sels)
-    
-    current_list = cmds.textScrollList(custom_outliner_list, q=True, allItems=True)
-    current_set = list(set(current_list))
-    
-    cmds.textScrollList(custom_outliner_list, e=True, removeAll=True)
-    cmds.textScrollList(custom_outliner_list, e=True, append=current_set)
+    if len(sels) > 0:
+        cmds.textScrollList(custom_outliner_list, e=True, append=sels)
+        
+        current_list = cmds.textScrollList(custom_outliner_list, q=True, allItems=True)
+        current_set = list(set(current_list))
+        
+        cmds.textScrollList(custom_outliner_list, e=True, removeAll=True)
+        cmds.textScrollList(custom_outliner_list, e=True, append=current_set)
     
 def outliner_remove(custom_outliner_list):
-    sels = cmds.ls(sl=True)
-    cmds.textScrollList(custom_outliner_list, e=True, removeItem=sels)
+    if cmds.textScrollList(custom_outliner_list, q=True, numberOfItems=True) > 0:
+        sels = cmds.ls(sl=True)
+        cmds.textScrollList(custom_outliner_list, e=True, removeItem=sels)
     
 def outliner_clear(custom_outliner_list):
     cmds.textScrollList(custom_outliner_list, e=True, removeAll=True)
 
 def outliner_sort(custom_outliner_list):
-    sorted_list = sorted(cmds.textScrollList(custom_outliner_list, q=True, allItems=True))
-    cmds.textScrollList(custom_outliner_list, e=True, removeAll=True)
-    cmds.textScrollList(custom_outliner_list, e=True, append=sorted_list)
+    if cmds.textScrollList(custom_outliner_list, q=True, numberOfItems=True) > 0:
+        sorted_list = sorted(cmds.textScrollList(custom_outliner_list, q=True, allItems=True))
+        cmds.textScrollList(custom_outliner_list, e=True, removeAll=True)
+        cmds.textScrollList(custom_outliner_list, e=True, append=sorted_list)
    
-   
- 
 def outliner_up(custom_outliner_list):
     sels = cmds.textScrollList(custom_outliner_list, q=True, selectItem=True)
     if cmds.textScrollList(custom_outliner_list, q=True, numberOfItems=True) > 0:
@@ -91,3 +109,14 @@ def outliner_down(custom_outliner_list):
                 cmds.textScrollList(custom_outliner_list, e=True, selectItem=sels)
                 
                 
+def outliner_select_by_type(custom_outliner_select_list, custom_outliner_list):
+    objInScene = cmds.ls()
+    type_list = cmds.textScrollList(custom_outliner_select_list, q=True, selectItem=True)
+    if type_list != None:
+        cmds.textScrollList(custom_outliner_list, e=True, removeAll=True)
+        
+        for obj in objInScene:
+            for type in type_list:
+                objType = cmds.objectType(obj)
+                if type == objType:
+                    cmds.textScrollList(custom_outliner_list, e=True, append=obj)
--- Maya_CyberDino/Scripting/Python/python/toolbox.py
+++ Maya_CyberDino/Scripting/Python/python/toolbox.py
@@ -13,6 +13,8 @@ import create_joints
 import create_controls
 import create_locator
 import custom_outliner
+import random_placement
+import custom_animation_window
 
 reload(rename_functions)
 reload(joint_functions)
@@ -23,10 +25,19 @@ reload(create_joints)
 reload(create_controls)
 reload(create_locator)
 reload(custom_outliner)
+reload(random_placement)
+reload(custom_animation_window)
 
 #my_toolbox_window()
 def create_my_toolbox():
-	main_column = cmds.columnLayout(adjustableColumn=True, columnAlign='center')
+	my_window = 'myToolboxWindow'
+    
+	if cmds.window(my_window, q=True, exists=True):
+	    cmds.deleteUI(my_window)
+
+	my_window = cmds.window(my_window, width=160, height=570)    
+    
+	main_column = cmds.columnLayout(parent=my_window, adjustableColumn=True, columnAlign='center')
     
 	image_row = cmds.rowLayout(parent=main_column, numberOfColumns=2, adjustableColumn=2, columnAlign=(2,'center'), backgroundColor=(1,1,1))
 	my_image = cmds.picture(image='logo-32.png', parent=image_row)
@@ -39,69 +50,73 @@ def create_my_toolbox():
 	
 	#Simple tools
 	cmds.separator(height=12, parent=tab_1_column)
-	delete_history_button = cmds.button(parent=tab_1_column, label='Delete History', command=lambda *args: simple_functions.delete_history())
-	freeze_transform_button = cmds.button(parent=tab_1_column, label='Freeze Transformations', command=lambda *args: simple_functions.freeze_transformations())
-	center_pivot_button = cmds.button(parent=tab_1_column, label='Center Pivot', command=lambda *args: simple_functions.center_pivot())
-	parent_scale_button = cmds.button(parent=tab_1_column, label='Parent and Scale Constrain', command=lambda *args: simple_functions.parent_scale_constrain())
+	delete_history_button = cmds.button(parent=tab_1_column, label='Delete History', annotation='Delete history', command=lambda *args: simple_functions.delete_history())
+	freeze_transform_button = cmds.button(parent=tab_1_column, label='Freeze Transformations', annotation='Freeze all transformations', command=lambda *args: simple_functions.freeze_transformations())
+	center_pivot_button = cmds.button(parent=tab_1_column, label='Center Pivot', annotation='Center pivot', command=lambda *args: simple_functions.center_pivot())
+	parent_scale_button = cmds.button(parent=tab_1_column, label='Parent and Scale Constrain', annotation='Parent and scale constrain', command=lambda *args: simple_functions.parent_scale_constrain())
 	
     #Poly Count
 	cmds.separator(height=12, parent=tab_1_column)
-	poly_count_button = cmds.button(parent=tab_1_column, label='Poly Count', command=lambda *args: simple_functions.toggle_poly_count())
+	poly_count_button = cmds.button(parent=tab_1_column, label='Poly Count', annotation='Toggle poly count', command=lambda *args: simple_functions.toggle_poly_count())
 	
 	#Orient Joint
 	cmds.separator(height=12, parent=tab_1_column)
 	orient_joint_row = cmds.rowLayout(parent=tab_1_column, numberOfColumns=2, columnWidth=(1, 125), adjustableColumn=2, columnAttach=(1, 'both', 0))
-	orient_joint_button = cmds.button(parent=orient_joint_row, label='Orient Joint Options', command=lambda *args: joint_functions.display_orient_joint_options())
-	display_axis_toggle = cmds.iconTextButton(style='iconOnly', image1='channelBoxUseManips.png', parent=orient_joint_row, command=lambda *args: joint_functions.display_local_axis())
-	orient_channelbox_button = cmds.button(parent=tab_1_column, label='Joint Attributes', command=lambda *args: joint_functions.show_joint_attr())
+	orient_joint_button = cmds.button(parent=orient_joint_row, label='Orient Joint Options', annotation='Display orient joint options', command=lambda *args: joint_functions.display_orient_joint_options())
+	display_axis_toggle = cmds.iconTextButton(style='iconOnly', image1='channelBoxUseManips.png', annotation='Toggle display axis for all joints', parent=orient_joint_row, command=lambda *args: joint_functions.display_local_axis())
+	orient_channelbox_button = cmds.button(parent=tab_1_column, label='Joint Attributes', annotation='Unhide more joint attributes', command=lambda *args: joint_functions.show_joint_attr())
 	
 	#Scale Compensation
 	cmds.separator(height=12, parent=tab_1_column)
 	scale_comp_label = cmds.text(label='Scale Compensation', parent=tab_1_column)
 	scale_comp_row = cmds.rowLayout(parent=tab_1_column, numberOfColumns=2, columnWidth=(1, 75), adjustableColumn=2, columnAttach=(1, "both", 0))
-	scale_comp_on_button = cmds.button(parent=scale_comp_row, label='On', command=lambda *args: joint_functions.scale_compensate(1))
-	scale_comp_off_button = cmds.button(parent=scale_comp_row, label='Off', command=lambda *args: joint_functions.scale_compensate(0))
+	scale_comp_on_button = cmds.button(parent=scale_comp_row, label='On', annotation='Turn scale compensation on', command=lambda *args: joint_functions.scale_compensate(1))
+	scale_comp_off_button = cmds.button(parent=scale_comp_row, label='Off', annotation='Turn scale compensation off', command=lambda *args: joint_functions.scale_compensate(0))
 	
 	#Covert to Joints
 	cmds.separator(height=12, parent=tab_1_column)
 	create_joints_row = cmds.rowLayout(parent=tab_1_column, numberOfColumns=2, columnWidth=(1, 110), adjustableColumn=2, columnAttach = (1, 'left', 0))
-	create_joints_button = cmds.button(parent=create_joints_row, label='Selection to Joints', command=lambda *args: create_joints.convert_to_joints(create_joints_checkbox))
-	create_joints_checkbox = cmds.checkBox(parent=create_joints_row, label='Del', align='right')
+	create_joints_button = cmds.button(parent=create_joints_row, label='Selection to Joints', annotation='Turn selection into joints', command=lambda *args: create_joints.convert_to_joints(create_joints_checkbox))
+	create_joints_checkbox = cmds.checkBox(parent=create_joints_row, label='Del', annotation='Toggle to replace selection with joints', align='right')
 
     #Place Controls
-	place_controls_button = cmds.button(parent=tab_1_column, label='Place Controls', command=lambda *args: create_controls.place_controls())
+	place_controls_button = cmds.button(parent=tab_1_column, label='Place Controls', annotation='Place controls at selection', command=lambda *args: create_controls.place_controls())
 	
 	#Locator
 	cmds.separator(height=12, parent=tab_1_column)
 	locator_row = cmds.rowLayout(parent=tab_1_column, numberOfColumns=3, columnWidth=(1, 62), adjustableColumn=2, columnAttach = (1, 'both', 0))
-	create_locator_button = cmds.button(parent=locator_row, label='Locators', command=lambda *args: create_locator.center_locator(locator_buttons))
+	create_locator_button = cmds.button(parent=locator_row, label='Locators', annotation='Create locator(s)', command=lambda *args: create_locator.center_locator(locator_buttons))
 	locator_buttons = cmds.radioCollection(parent=locator_row)
-	locator_button_1 = cmds.radioButton(label='Each')
-	locator_button_2 = cmds.radioButton(label='All')
+	locator_button_1 = cmds.radioButton(label='Each', annotation='Create locator at each selection')
+	locator_button_2 = cmds.radioButton(label='All', annotation='Create locator at center of entire selection')
 	cmds.radioCollection(locator_buttons, edit=True, select=locator_button_1)
 	
 	#Rename Functions
 	cmds.separator(height=12, parent=tab_1_column)
-	rename_button = cmds.button(parent=tab_1_column, label='Rename', command=lambda *args: rename_functions.rename_prompt())
-	search_and_replace_button = cmds.button(parent=tab_1_column, label='Search And Replace', command=lambda *args: rename_functions.search_and_replace())
+	rename_button = cmds.button(parent=tab_1_column, label='Rename', annotation='Sequential renamer', command=lambda *args: rename_functions.rename_prompt())
+	search_and_replace_button = cmds.button(parent=tab_1_column, label='Search And Replace', annotation='Search and replace', command=lambda *args: rename_functions.search_and_replace())
 
 	#Auto rigs
 	cmds.separator(height=12, parent=tab_1_column)
 	auto_rigs_row = cmds.rowLayout(parent=tab_1_column, numberOfColumns=4, columnAlign=(2, 'left'))
-	fk_button = cmds.iconTextButton(style='iconAndTextVertical', image1='kinJoint.png', label='FK', parent= auto_rigs_row, command=lambda *args: fk_systems.simple_fk())
-	broken_fk_button = cmds.iconTextButton(style='iconAndTextVertical', image1='kinJoint.png', label='Broken', parent= auto_rigs_row, command=lambda *args: fk_systems.broken_fk())
-	ik_button = cmds.iconTextButton(style='iconAndTextVertical', image1='kinHandle.png', label='IK', parent= auto_rigs_row, command='')
-	rk_button = cmds.iconTextButton(style='iconAndTextVertical', image1='kinJoint.png', label='RK', parent= auto_rigs_row, command='')
+	fk_button = cmds.iconTextButton(style='iconAndTextVertical', image1='kinJoint.png', label='FK', annotation='Creates an FK rig. Select first joint of chain then run', parent= auto_rigs_row, command=lambda *args: fk_systems.simple_fk())
+	broken_fk_button = cmds.iconTextButton(style='iconAndTextVertical', image1='kinJoint.png', label='Broken', annotation='Creates an broken FK rig. Select first joint of chain then run', parent= auto_rigs_row, command=lambda *args: fk_systems.broken_fk())
+	ik_button = cmds.iconTextButton(style='iconAndTextVertical', image1='kinHandle.png', label='IK', annotation='Creates an IK rig. Select first joint of chain then run', parent= auto_rigs_row, command='')
+	rk_button = cmds.iconTextButton(style='iconAndTextVertical', image1='kinJoint.png', label='RK', annotation='Creates an RK rig. Select first joint of chain then run', parent= auto_rigs_row, command='')
 	
 	#Outliner
 	cmds.separator(height=12, parent=tab_1_column)
-	custom_outliner_button = cmds.button(parent=tab_1_column, label='Custom Outliner', command=lambda *args: custom_outliner.custom_outliner())
+	custom_outliner_button = cmds.button(parent=tab_1_column, label='Custom Outliner', annotation='Open custom outliner', command=lambda *args: custom_outliner.custom_outliner())
+	
+	#Animation Window
+	cmds.separator(height=12, parent=tab_1_column)
+	custom_outliner_button = cmds.button(parent=tab_1_column, label='Animator', annotation='Open animation master', command=lambda *args: custom_animation_window.custom_animation())
 	
 	#####tab 2
 	tab_2_column = cmds.columnLayout(adjustableColumn=True, columnAlign='center', parent=my_tabs)
 	
 	cmds.separator(height=12, parent=tab_2_column)
-	random_placement_button = cmds.button(parent=tab_2_column, label='Random Placement', command='')
+	random_placement_button = cmds.button(parent=tab_2_column, label='Random Placement', annotation='Random placement options', command=lambda *args: random_placement.random_placement_prompt())
 	
 	cmds.separator(height=12, parent=tab_2_column)
 	other_label = cmds.text(label='...', parent=tab_2_column)
@@ -110,6 +125,7 @@ def create_my_toolbox():
 	
 	date = cmds.text(label='2017', parent=main_column, align='right')
 	
+	cmds.showWindow(my_window)
     
 def my_toolbox_window():
     my_workspace  = 'My Workspace'
@@ -117,7 +133,9 @@ def my_toolbox_window():
     if cmds.workspaceControl(my_workspace, exists=True):
         cmds.workspaceControl(my_workspace, e=True, close=True)
         
-    my_workspace = cmds.workspaceControl(my_workspace, retain=False, floating=False, label='Cody', widthProperty='preferred', initialWidth=160, initialHeight=560, resizeWidth=160, resizeHeight=560, uiScript='create_my_toolbox()')
+    my_workspace = cmds.workspaceControl(my_workspace, retain=False, floating=False, label='Cody', widthProperty='preferred', 
+        initialWidth=160, initialHeight=570, resizeWidth=160, resizeHeight=560, uiScript='create_my_toolbox()')
 
-
-my_toolbox_window()
+#print(cmds.about(version=True))
+create_my_toolbox()
+#my_toolbox_window()
